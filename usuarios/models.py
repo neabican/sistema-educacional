9f934c46e03fdbd188ab08cb808681fe9f516e64
Eduardo <edu-henrique2014@hotmail.com>
2021-12-04 19:01:04 -0300

    App de usuarios criado

---
+++ usuarios/models.py
@@ -0,0 +1,9 @@
+# from django.contrib.auth.models import AbstractUser
+# from django.conf import settings
+# from django.db import models
+
+# class User(AbstractUser):
+  
+
+#   def __str__(self):
+#     return self.email 
