8f6764e314c6d6832152fd906d6bd5063c37a754
Eduardo <edu-henrique2014@hotmail.com>
2021-12-05 10:55:38 -0300

    base.html alterado e pastas de views configurada

---
+++ cadastros/views/dashboard.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+def dashboard(request):
+  return render(request, 'dashboard.html')
--- sistema_educacional/urls.py
+++ sistema_educacional/urls.py
@@ -1,6 +1,12 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from cadastros import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('cadastros.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
