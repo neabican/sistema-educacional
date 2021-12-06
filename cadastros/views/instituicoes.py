c5b5da44448ba6f9226567af67cf8bec0a8296dd
Eduardo <edu-henrique2014@hotmail.com>
2021-12-06 10:07:34 -0300

    Snippet de paginacao de tabelas criado

--- cadastros/views/instituicoes.py
+++ cadastros/views/instituicoes.py
@@ -3,9 +3,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from ..models import Instituicao, Campus
+from .utilitarios import gerar_paginacao
 
 @login_required
 def instituicoes(request):
   instituicoes = Instituicao.objects.all()
+  instituicoes, paginas = gerar_paginacao(request, instituicoes, 10)
 
-  return render(request, 'cadastros/instituicoes/listagem.html', { 'instituicoes': instituicoes })
+  return render(request, 'cadastros/instituicoes/listagem.html', { 
+    'instituicoes': instituicoes, 'paginas': paginas
+  })
---
+++ cadastros/views/utilitarios.py
@@ -0,0 +1,27 @@
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
+
+def gerar_paginacao(request, resultados, num_resultados):
+  paginator = Paginator(resultados, num_resultados)
+
+  try:
+    page = int(request.GET.get('page', '1'))
+  except:
+    page = 1
+  try:
+    resultados_paginados = paginator.page(page)
+  except(EmptyPage, InvalidPage):
+    resultados_paginados = paginator.page(paginator.num_pages)
+
+  paginas = {}
+  
+  paginas['total'] = paginator.num_pages - 2
+  paginas['ultimo'] = paginator.num_pages
+  paginas['anterior'] = 0
+  paginas['proximo'] = page + 1
+
+  if page >= 3:
+    paginas['anterior'] = page - 1
+  elif page == 1:
+    paginas['proximo'] = page + 2
+
+  return resultados_paginados, paginas
