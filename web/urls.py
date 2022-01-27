569b44493e9ceb45aa0b01945e2c12dc92eecca2
Eduardo <edu-henrique2014@hotmail.com>
2022-01-27 19:33:53 -0300

    Página de detalhes do câmpus implementada

--- web/urls.py
+++ web/urls.py
@@ -5,4 +5,5 @@ app_name = 'web'
 
 urlpatterns = [
   path('', views.listar_campus, name='listar_campus'),
+  path('campus/<int:pk>', views.detalhes_campus, name='detalhes_campus'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from cadastros.models import Campus, Programa, Projeto, AcaoAfirmativa
 
 def listar_campus(request):
   return render(request, 'web/listar_campus.html')
+
+def detalhes_campus(request, pk):
+  campus = get_object_or_404(Campus, pk=pk)
+
+  programas = Programa.objects.filter(campus=campus)
+  projetos = Projeto.objects.filter(campus=campus)
+  acoes_afirmativas = AcaoAfirmativa.objects.filter(campus=campus)
+  
+  return render(request, 'web/detalhes_campus.html', {
+    'campus': campus, 'programas': programas,
+    'projetos': projetos, 'acoes_afirmativas': acoes_afirmativas
+  })
