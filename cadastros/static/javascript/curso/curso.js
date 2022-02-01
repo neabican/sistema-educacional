33dbe3d9b9e822397450a569b39a9e84b5f4b135
Eduardo-H <edu-henrique2014@hotmail.com>
2022-02-01 15:03:48 -0300

    Erro na listagem de cursos arrumado

--- cadastros/static/javascript/curso/curso.js
+++ cadastros/static/javascript/curso/curso.js
@@ -1,6 +1,8 @@
 'use strict';
 
-function abrirModalInformacoes(nome, descricao) {
+function abrirModalInformacoes(nome, id) {
   document.getElementById('nome-curso').textContent = nome;
+
+  const descricao = document.getElementById(`descricao-${id}`).innerText;
   document.getElementById('descricao-curso').innerHTML = descricao;
 }
