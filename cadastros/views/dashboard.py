d41ae6cfb448432c6296f2d59e4e88012496e537
Eduardo-H <edu-henrique2014@hotmail.com>
2022-02-01 20:22:20 -0300

    Conteúdo adicionado na tela de dashboard

--- cadastros/views/dashboard.py
+++ cadastros/views/dashboard.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from ..models import Instituicao, Campus, Curso
+
 @login_required
 def dashboard(request):
-  return render(request, 'dashboard.html')
+  instituicoes = Instituicao.objects.count()
+  campus = Campus.objects.count()
+  cursos = Curso.objects.count()
+
+  dados = {
+    'instituicoes': instituicoes,
+    'campus': campus,
+    'cursos': cursos
+  }
+
+  return render(request, 'dashboard.html', {'dados': dados})
