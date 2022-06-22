7101e146cc6c975025e9c26f83d1becca4efa3f9
Gustavo Ziger <44277841+zi-ger@users.noreply.github.com>
2022-06-22 18:55:58 -0300

    Atualizar programas.py
    Remoção da necessidade de token de autenticação.

--- api/views/programas.py
+++ api/views/programas.py
@@ -9,8 +9,6 @@ from ..serializers import ProgramaSerializer
 from cadastros.models import *
 
 @api_view(['GET'])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
 def listar_programas(request, pk_campus):
   if request.method == 'GET':
     programas = Programa.objects.filter(campus__pk=pk_campus)
@@ -19,8 +17,6 @@ def listar_programas(request, pk_campus):
     return Response(serializador.data)
 
 @api_view(['GET'])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
 def detalhes_programa(request, pk, pk_campus):
   if request.method == 'GET':
     try:
