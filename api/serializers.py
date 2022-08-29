4d5b9e05bb0fe7119b5628a013c3d532b91ee20b
Gustavo Ziger <44277841+zi-ger@users.noreply.github.com>
2022-08-29 14:17:48 -0300

    Atualizar serializers.py
    Adiciona o serializador CampusSerializerSemInstituicao e altera o da Instituição

--- api/serializers.py
+++ api/serializers.py
@@ -2,14 +2,19 @@ from rest_framework import serializers
 from cadastros.models import *
 
 class InstituicaoSerializer(serializers.ModelSerializer):
+  campus = serializers.SerializerMethodField()
+
+  def get_campus(self, obj):
+    return CampusSerializerSemInstituicao(Campus.objects.filter(instituicao=obj), many=True).data
+
   class Meta:
     model = Instituicao
-    fields = ['pk', 'nome', 'sigla']
+    fields = ['pk', 'nome', 'sigla', 'campus']
 
 class CursoSerializer(serializers.ModelSerializer):
   class Meta:
     model = Curso
-    fields = ['pk','nome', 'descricao']
+    fields = ['pk', 'nome', 'descricao']
 
 class CursoCampusSerializer(serializers.ModelSerializer):
   curso = CursoSerializer()
@@ -30,7 +35,7 @@ class ProgramaSerializer(serializers.ModelSerializer):
   class Meta:
     model = Programa
     fields = [
-      'pk', 'nome', 'descricao', 
+      'pk', 'nome', 'descricao',
       'link', 'campus'
     ]
 
@@ -38,7 +43,7 @@ class ProjetoSerializer(serializers.ModelSerializer):
   class Meta:
     model = Projeto
     fields = [
-      'pk', 'nome', 'descricao', 
+      'pk', 'nome', 'descricao',
       'link', 'campus'
     ]
 
@@ -46,12 +51,36 @@ class AcaoAfirmativaSerializer(serializers.ModelSerializer):
   class Meta:
     model = AcaoAfirmativa
     fields = [
-      'pk', 'nome', 'descricao', 
+      'pk', 'nome', 'descricao',
       'link', 'campus'
     ]
 
 class CampusSerializer(serializers.ModelSerializer):
   instituicao = InstituicaoSerializer()
+  endereco = EnderecoSerializer()
+  cursos = CursoCampusSerializer(many=True)
+  programas = serializers.SerializerMethodField()
+  projetos = serializers.SerializerMethodField()
+  acoes_afirmativas = serializers.SerializerMethodField()
+
+  def get_programas(self, obj):
+    return ProgramaSerializer(Programa.objects.filter(campus=obj), many=True).data
+
+  def get_projetos(self, obj):
+    return ProjetoSerializer(Projeto.objects.filter(campus=obj), many=True).data
+
+  def get_acoes_afirmativas(self, obj):
+    return AcaoAfirmativaSerializer(AcaoAfirmativa.objects.filter(campus=obj), many=True).data
+
+  class Meta:
+    model = Campus
+    fields = [
+      'pk', 'nome', 'foto', 'instituicao',
+      'endereco', 'cursos', 'programas',
+      'projetos', 'acoes_afirmativas',
+    ]
+
+class CampusSerializerSemInstituicao(serializers.ModelSerializer):
   endereco = EnderecoSerializer()
   cursos = CursoCampusSerializer(many=True)
   programas = serializers.SerializerMethodField()
