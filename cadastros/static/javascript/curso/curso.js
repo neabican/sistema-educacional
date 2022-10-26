6ef2547d16d5ed55d51d894ff7cf6899440a3e2b
Gustavo Ziger <gustavoziger@gmail.com>
2022-10-26 12:49:03 -0300

    Adicionado o campo de vagas na descrição do curso na listagem

--- cadastros/static/javascript/curso/curso.js
+++ cadastros/static/javascript/curso/curso.js
@@ -1,7 +1,8 @@
 'use strict';
 
-function abrirModalInformacoes(nome, id) {
+function abrirModalInformacoes(nome, id, vagas) {
   document.getElementById('nome-curso').textContent = nome;
+  document.getElementById('vagas-curso').textContent = vagas;
 
   const descricao = document.getElementById(`descricao-${id}`).innerText;
   document.getElementById('descricao-curso').innerHTML = descricao;
